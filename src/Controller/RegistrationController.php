9281dff7446fd4f4c2e96d57f4262a4adeaee917
Thierry7486 <thierrymalherbe@live.fr>
2021-09-16 21:46:52 +0200

    mise en place du controller public et des templates

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('menu_index');
+            return $this->redirectToRoute('public');
         }
 
         return $this->render('registration/register.html.twig', [
